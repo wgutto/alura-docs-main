ed4fecf0d076fe34c9c78ec3a0a8355c68880256
Augusto Brito <augusstobrito@gmail.com>
2026-06-26 22:14:59 -0300

    cria/configura o servidor express

---
+++ src/servidor.js
@@ -0,0 +1,15 @@
+import express from "express";
+import url from "url";
+import path from "path";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
+app.use(express.static(diretorioPublico));
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+})
