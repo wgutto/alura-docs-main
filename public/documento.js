5d06aeb38aaec11d4e3ee635d34fae001db0834b
Augusto Brito <augusstobrito@gmail.com>
2026-06-28 22:53:09 -0300

    cria os eventos do front/back que registra o valor

--- public/documento.js
+++ public/documento.js
@@ -2,6 +2,11 @@ const socket = io();
 
 const textoEditor = document.getElementById("editor-texto");
 
+// O evento "keyup" é acionado sempre que o usuário solta uma tecla enquanto digita no campo de texto. Quando isso acontece, o valor atual do campo de texto é enviado para o servidor usando o socket.emit, com o evento "texto_editor" e o valor do campo como dados.
 textoEditor.addEventListener("keyup", () => {
   socket.emit("texto_editor", textoEditor.value);
-})
+});
+
+socket.on("texto_editor_clientes", (texto) => {
+    textoEditor.value = texto;
+  })
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,11 @@
 import io from "./servidor.js";
 
-// O io.on é usado para ouvir eventos de conexão de clientes. Quando um novo cliente se conecta ao servidor, a função de callback (socket) é executada, e o ID do socket do cliente é registrado no console.
+// O io.on é usado para ouvir eventos de conexão de clientes. Quando um novo cliente se conecta, a função de callback é executada, e o ID do cliente conectado é registrado no console.
 io.on("connection", (socket) => {
   console.log("Novo cliente conectado ID:", socket.id);
 
-  socket.on("texto_editor", (socket) => {
-    console.log("Texto recebido do cliente:", socket);
+  // O socket.on é usado para ouvir eventos específicos enviados pelo cliente. Neste caso, ele está ouvindo o evento "texto_editor". Quando esse evento é recebido, a função de callback é executada, e o valor do texto enviado pelo cliente é registrado no console.
+  socket.on("texto_editor", (texto) => {
+    socket.broadcast.emit("texto_editor_clientes", texto);
   })
 })
