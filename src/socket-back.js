9bf732513b8e7b62f6699148a03a8e20e15a13a8
Augusto Brito <augusstobrito@gmail.com>
2026-06-28 22:29:01 -0300

    cria o evento texto_editor do socket io

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,6 +1,10 @@
 import io from "./servidor.js";
 
-
+// O io.on é usado para ouvir eventos de conexão de clientes. Quando um novo cliente se conecta ao servidor, a função de callback (socket) é executada, e o ID do socket do cliente é registrado no console.
 io.on("connection", (socket) => {
   console.log("Novo cliente conectado ID:", socket.id);
+
+  socket.on("texto_editor", (socket) => {
+    console.log("Texto recebido do cliente:", socket);
+  })
 })
